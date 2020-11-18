d6a5d6218b29338681db38598f85c52ee4e580dd
Bernd Paysan <bernd.paysan@gmx.de>
2020-11-18 15:16:37 +0100

    Remove workaround for gcc 10.x

--- ed25519-prims.h
+++ ed25519-prims.h
@@ -25,29 +25,29 @@ typedef struct ge25519_niels_t {
 	bignum25519 ysubx, xaddy, t2d;
 } ge25519_niels;
 
-void ge25519_pack(unsigned char r[32], const ge25519 *p);
-int ge25519_unpack_negative_vartime(ge25519 *r, const unsigned char p[32]);
-void ge25519_add(ge25519 *r, const ge25519 *p,  const ge25519 *q);
-void ge25519_double_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const bignum256modm s1, const bignum256modm s2);
-void ge25519_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const bignum256modm s1);
-void ge25519_scalarmult(ge25519 *r, const ge25519 *p1, const bignum256modm s1);
-void ge25519_scalarmult_base(ge25519 *r, const bignum256modm s);
-void expand256_modm(bignum256modm out, const unsigned char *in, size_t len);
-void expand_raw256_modm(bignum256modm out, const unsigned char in[32]);
-void contract256_modm(unsigned char out[32], const bignum256modm in);
-void add256_modm(bignum256modm r, const bignum256modm x, const bignum256modm y);
-void mul256_modm(bignum256modm r, const bignum256modm x, const bignum256modm y);
-void sub256_modm_batch(bignum256modm out, const bignum256modm a, const bignum256modm b, size_t limbsize);
-void invert256_modm(bignum256modm recip, const bignum256modm s);
-int lt256_modm_batch(const bignum256modm a, const bignum256modm b, size_t limbsize);
-int lte256_modm_batch(const bignum256modm a, const bignum256modm b, size_t limbsize);
-int iszero256_modm_batch(const bignum256modm a);
-int isone256_modm_batch(const bignum256modm a);
+extern void ge25519_pack(unsigned char r[32], const ge25519 *p);
+extern int ge25519_unpack_negative_vartime(ge25519 *r, const unsigned char p[32]);
+extern void ge25519_add(ge25519 *r, const ge25519 *p,  const ge25519 *q);
+extern void ge25519_double_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const bignum256modm s1, const bignum256modm s2);
+extern void ge25519_scalarmult_vartime(ge25519 *r, const ge25519 *p1, const bignum256modm s1);
+extern void ge25519_scalarmult(ge25519 *r, const ge25519 *p1, const bignum256modm s1);
+extern void ge25519_scalarmult_base(ge25519 *r, const bignum256modm s);
+extern void expand256_modm(bignum256modm out, const unsigned char *in, size_t len);
+extern void expand_raw256_modm(bignum256modm out, const unsigned char in[32]);
+extern void contract256_modm(unsigned char out[32], const bignum256modm in);
+extern void add256_modm(bignum256modm r, const bignum256modm x, const bignum256modm y);
+extern void mul256_modm(bignum256modm r, const bignum256modm x, const bignum256modm y);
+extern void sub256_modm_batch(bignum256modm out, const bignum256modm a, const bignum256modm b, size_t limbsize);
+extern void invert256_modm(bignum256modm recip, const bignum256modm s);
+extern int lt256_modm_batch(const bignum256modm a, const bignum256modm b, size_t limbsize);
+extern int lte256_modm_batch(const bignum256modm a, const bignum256modm b, size_t limbsize);
+extern int iszero256_modm_batch(const bignum256modm a);
+extern int isone256_modm_batch(const bignum256modm a);
 
-const ge25519 ge25519_basepoint;
-const bignum25519 ge25519_ecd;
-const bignum25519 ge25519_ec2d;
-const bignum25519 ge25519_sqrtneg1;
-const ge25519_niels ge25519_niels_sliding_multiples[32];
+extern const ge25519 ge25519_basepoint;
+extern const bignum25519 ge25519_ecd;
+extern const bignum25519 ge25519_ec2d;
+extern const bignum25519 ge25519_sqrtneg1;
+extern const ge25519_niels ge25519_niels_sliding_multiples[32];
 
 #endif
